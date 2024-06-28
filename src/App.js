944481dba1c53bd36db2a70b5d8b8b3d8c5a6dcc
Triloksoni12 <triloksoni222@gmail.com>
2024-06-28 15:37:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDolist from './components/ToDolist';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my To Do App</h1>
+      <ToDolist/>
+    </div>
+  );
+}
+
+export default App;
